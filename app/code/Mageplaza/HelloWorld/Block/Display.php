2ae6e49b717ee177b606c0e2e317ddd6c130e3f3
bharat-hbswl <bharat.upadhyay@hbwsl.com>
2024-08-30 15:16:35 +0530

    Link 6 updated

--- app/code/Mageplaza/HelloWorld/Block/Display.php
+++ app/code/Mageplaza/HelloWorld/Block/Display.php
@@ -2,13 +2,23 @@
 namespace Mageplaza\HelloWorld\Block;
 class Display extends \Magento\Framework\View\Element\Template
 {
-	public function __construct(\Magento\Framework\View\Element\Template\Context $context)
+	protected $_postFactory;
+	public function __construct(
+		\Magento\Framework\View\Element\Template\Context $context,
+		\Mageplaza\HelloWorld\Model\PostFactory $postFactory
+	)
 	{
+		$this->_postFactory = $postFactory;
 		parent::__construct($context);
 	}
 
 	public function sayHello()
 	{
 		return __('Hello World');
+	}
+
+	public function getPostCollection(){
+		$post = $this->_postFactory->create();
+		return $post->getCollection();
 	}
 }
