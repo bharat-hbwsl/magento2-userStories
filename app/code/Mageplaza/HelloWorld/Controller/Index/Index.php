4178b13af9ce377d32c81a32f6417a0cb9f87c36
bharat-hbswl <bharat.upadhyay@gmail.com>
2024-08-30 09:50:04 +0530

    CRUD Operations link work

---
+++ app/code/Mageplaza/HelloWorld/Controller/Index/Index.php
@@ -0,0 +1,34 @@
+<?php
+namespace Mageplaza\HelloWorld\Controller\Index;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+	protected $_pageFactory;
+
+	protected $_postFactory;
+
+	public function __construct(
+		\Magento\Framework\App\Action\Context $context,
+		\Magento\Framework\View\Result\PageFactory $pageFactory,
+		\Mageplaza\HelloWorld\Model\PostFactory $postFactory
+		)
+	{
+		$this->_pageFactory = $pageFactory;
+		$this->_postFactory = $postFactory;
+		return parent::__construct($context);
+	}
+
+	public function execute()
+	{
+        echo "HelloWorld";
+		$post = $this->_postFactory->create();
+		$collection = $post->getCollection();
+		foreach($collection as $item){
+			echo "<pre>";
+			print_r($item->getData());
+			echo "</pre>";
+		}
+		exit();
+		return $this->_pageFactory->create();
+	}
+}
