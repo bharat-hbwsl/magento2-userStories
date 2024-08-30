827bfe846e9205eecfbf6419e2aad388d7c7a8d9
bharat-hbswl <bharat.upadhyay@hbwsl.com>
2024-08-30 12:23:10 +0530

    Link 5 Block layout template till HelloWorld

---
+++ app/code/Mageplaza/HelloWorld/Block/Display.php
@@ -0,0 +1,14 @@
+<?php
+namespace Mageplaza\HelloWorld\Block;
+class Display extends \Magento\Framework\View\Element\Template
+{
+	public function __construct(\Magento\Framework\View\Element\Template\Context $context)
+	{
+		parent::__construct($context);
+	}
+
+	public function sayHello()
+	{
+		return __('Hello World');
+	}
+}
