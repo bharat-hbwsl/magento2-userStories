8b59ca0b7e1691054ed54baf0d77446ce3006b6c
bharat-hbswl <bharat.upadhyay@hbwsl.com>
2024-09-18 17:38:21 +0530

    User story 11 - Akeneo Setup

--- app/etc/config.php
+++ app/etc/config.php
@@ -346,6 +346,7 @@ return [
         'Magento_WishlistAnalytics' => 1,
         'Magento_WishlistGraphQl' => 1,
         'Magento_WishlistSampleData' => 1,
+        'Akeneo_Connector' => 1,
         'Bharat_Checkout' => 0,
         'Bharat_HelloWorld' => 1,
         'Bharat_Mod1' => 1,
